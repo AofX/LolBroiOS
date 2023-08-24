5479f0301a67e5e9a5fb1e5e5f8ce0efc1d4a04f
Kevin Jimenez <elestevy@gmail.com>
2023-08-23 20:13:49 -0700

    Initial Commit

---
+++ LoLBro/LoLBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoLBroApp.swift
+//  LoLBro
+//
+//  Created by Kevin Jimenez on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LoLBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
